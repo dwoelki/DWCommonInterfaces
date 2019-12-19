fb1a69c937cd48e26ab85a3e8daf355fb70c9da2
d <doom@ls-clan.net>
2019-12-19 19:46:14 +0100

    interface IOrdinaryObject for arbitrary, unspecified purposes

---
+++ src/de/woelki_web/commons/IOrdinaryObject.java
@@ -0,0 +1,17 @@
+/* CHANGELOG
+ * 2019-12-19   DW  Creation as empty interface
+ */
+package de.woelki_web.commons;
+
+/**
+ * Interface-description:<br>
+ * This interface does effecitvely nothing.<br>
+ * Its purpose is to use at as type specification for arbitrary purposes,
+ * e.g. when initializing any {@link java.lang.Object} extending classes via
+ * Lookup/META-INF services.
+ * 
+ * @author D. Woelki, doom@ls-clan.net
+ */
+public interface IOrdinaryObject {
+    
+}
